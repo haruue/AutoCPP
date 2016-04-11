e73a469e45bc487a0b7ba7de2ed3d49cd70fc256
Haruue Icymoon <haruue@caoyue.com.cn>
2016-04-11 12:51:09 +0800

    优化代码

--- AutoCPP.js
+++ AutoCPP.js
@@ -3,26 +3,19 @@
 
 var $f3 = window.frames["topmain"].frames["main"];
 
-function numToChoice (num) {
-	switch (num) {
-		case 0: return 'A';
-		case 1: return 'B';
-		case 2: return 'C';
-		case 3: return 'D';
-	}
-}
+var choices=new Array('A', 'B', 'C', 'D');
 
 function getAnswer() {
 	for (var i = 0; i <= 3; i++) {
-		var vTestParam = '<cTestParam><cQuestion>' + $f3.cQuestionID.value + '</cQuestion><cUserAnswer>' + numToChoice(i) + '</cUserAnswer></cTestParam>';
+		var vTestParam = '<cTestParam><cQuestion>' + $f3.cQuestionID.value + '</cQuestion><cUserAnswer>' + choices[i] + '</cUserAnswer></cTestParam>';
 		var isTrue = $f3.CExam.CPractice.IsOrNotTrue(vTestParam);
-		if(isTrue.value) return numToChoice(i);
+		if(isTrue.value) return choices[i];
 	}
 }
 
-function choice() {
+function choose() {
 	$f3.makeChoice(getAnswer());
-	setTimeout("choice()", 2700);
+	setTimeout("choose()", 2700);
 }
 
-choice();
+choose();
